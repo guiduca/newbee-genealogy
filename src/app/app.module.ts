296ae6512e5b3646c5b809ba091c83f6e7d0f577
Odan <audran.guiducci@epitech.eu>
2018-10-02 16:52:53 +0800

    beggining of genealogy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FamillyMemberComponent } from './familly-member/familly-member.component';
+import { FamillyTreeComponent } from './familly-tree/familly-tree.component';
+import { GenerationComponent } from './generation/generation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FamillyMemberComponent,
+    FamillyTreeComponent,
+    GenerationComponent
   ],
   imports: [
     BrowserModule
